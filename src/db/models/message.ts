c809064911a183bfb850f2484b64099c44e2d6de
Adrian Gomez <aegomez@tekmexico.com>
2020-01-10 16:43:02 -0800

    Update message model

--- src/db/models/message.ts
+++ src/db/models/message.ts
@@ -2,6 +2,10 @@ import { createSchema, Type, typedModel, ExtractProps } from 'ts-mongoose';
 
 const isRequired = { required: true as const };
 
+// The message sent/delivered status.
+export const deliveryEnum = ['pending', 'sent', 'delivered', 'seen'] as const;
+export type DeliveryStatus = typeof deliveryEnum[number];
+
 export const MessageSchema = createSchema({
   author: Type.string(isRequired),
   content: Type.string(isRequired),
@@ -12,10 +16,14 @@ export const MessageSchema = createSchema({
   visible: Type.boolean({
     ...isRequired,
     default: true
+  }),
+  delivery: Type.string({
+    ...isRequired,
+    enum: deliveryEnum,
+    default: deliveryEnum[0]
   })
 });
 
 export const Message = typedModel('Message', MessageSchema);
 
 export type MessageProps = ExtractProps<typeof MessageSchema>;
-export type InitMessageProps = Pick<MessageProps, 'author' | 'content'>;
